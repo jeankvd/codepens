7b55707a20b28790b3ac3f0156c69e9979e2675b
jeankvd <djjean85@gmail.com>
2017-11-14 21:52:54 -0500

    "Width changes on click overlay"

--- Wolf Slider/app.js
+++ Wolf Slider/app.js
@@ -1,6 +1,8 @@
 let clickPoint = document.getElementById("hero");
+let overlay = document.getElementById("overlay");
+
 
 clickPoint.addEventListener("click", (e) => {
   console.log(e.screenX);
+  overlay.style.width = e.screenX + "px";
 });
-console.log(clickPoint);
