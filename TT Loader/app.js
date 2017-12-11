670b7292d20228811a5fa69dc47fc700f7c183d0
jeankvd <djjean85@gmail.com>
2017-12-10 22:06:14 -0500

    "Finished TT loader"

--- TT Loader/app.js
+++ TT Loader/app.js
@@ -1,12 +1,11 @@
-aconst rightDiv = document.getElementById("loading-overlay-right");
+const rightDiv = document.getElementById("loading-overlay-right");
 const leftDiv = document.getElementById("loading-overlay-left");
 const overlay = document.getElementById("overlay");
 const logo = document.getElementById("logo");
 const loadingText = document.getElementById("loading-text");
 
-
 function startLoadingClass(e) {
-	logo.classList.add("started");
+	// logo.classList.add("started");
 };
 
 function loading() {
@@ -32,3 +31,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
     setTimeout(endLoadingClass, 4000);
 		setTimeout(overlayEndLoading, 5000);
   });
+
+	
